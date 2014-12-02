61f086054150878c6fda0892db92cb916d49631f
david bigagli <bokassa@gmail.com>
2014-12-02 12:25:59 -0800

    Add a compatibility define for tcl.

--- lsf/intlib/lsftcl.c
+++ lsf/intlib/lsftcl.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  */
-
+#define USE_INTERP_RESULT 1
 #include "../lsf.h"
 #include "resreq.h"
 #if defined(HAVE_TCL_TCL_H)
