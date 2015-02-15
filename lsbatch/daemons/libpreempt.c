6183adfaca7fb5414cff676c2b1b325c2a0b2b11
David Bigagli <david@openlava.org>
2015-02-14 20:04:45 -0800

    Add preemption plugin files.

---
+++ lsbatch/daemons/libpreempt.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 David Bigagli
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301, USA
+ *
+ */
+#include "preempt.h"
+
+/* prm_init()
+ */
+int
+prm_init(LIST_T *qList)
+{
+    return 0;
+}
+
+int
+prm_elect_preempt(struct qData *qPtr, link_t **l)
+{
+    return 0;
+}
