293bfc053f9814544823029a7e7f258a479d3024
David Bigagli <david@openlava.org>
2015-10-07 14:23:34 +0200

    Increase DEF_SBD_MAX_CONNS for high performace throughput.

--- lsbatch/daemons/mbd.h
+++ lsbatch/daemons/mbd.h
@@ -41,7 +41,7 @@
 #define  DEF_EXCLUSIVE        FALSE
 #define  DEF_EVENT_WATCH_TIME 60
 #define  DEF_COND_CHECK_TIME  600
-#define DEF_MAX_SBD_CONNS     774
+#define DEF_MAX_SBD_CONNS     4096  /* Increase in lsb.params for high performance */
 #define DEF_SCHED_STAY        3
 #define DEF_FRESH_PERIOD     15
 #define DEF_PEND_EXIT       512
