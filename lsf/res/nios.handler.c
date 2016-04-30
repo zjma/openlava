1b27bd3ad51e7ebc77355d593c83b316f078d4d0
Weina Ma <vena1028@gmail.com>
2016-04-30 10:02:58 +0200

    Fix res/nios hang

--- lsf/res/nios.handler.c
+++ lsf/res/nios.handler.c
@@ -478,8 +478,13 @@ ls_nioselect(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
                     xdrmem_create(&xdrs, (char *) &bufHdr,
                                   sizeof(struct LSFHeader), XDR_DECODE);
 
-                    if (readDecodeHdr_(conn[i].sock.fd, (char *) &bufHdr,
-                                       NB_SOCK_READ_FIX, &xdrs, &msgHdr) < 0) {
+                    if ((retVal = readDecodeHdr_(conn[i].sock.fd, (char *) &bufHdr,
+                                       NB_SOCK_READ_FIX, &xdrs, &msgHdr)) < 0) {
+                        //skip: select again
+                        if (retVal == -3) {
+                            xdr_destroy(&xdrs);
+                            continue;
+                        }
                         if (!conn[i].dead) {
                             memset((void *)&status, 0, sizeof(LS_WAIT_T));
                             SETTERMSIG(status, STATUS_IOERR);
