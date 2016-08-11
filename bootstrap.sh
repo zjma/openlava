d96177624a2085471ac4a37d6219421a5a4521db
David Bigagli <david@openlava.org>
2016-08-11 15:33:14 +0200

    Create the revision file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,11 @@ autoheader
 automake --add-missing --copy
 #set +x
 #test -n "$NOCONFIGURE" || ./configure "$@"
+if [ ! -d ".git" ]; then
+  /bin/echo "No .git directory, no commit file"
+  exit 0
+fi
+
+# git command better be in your path
+git rev-parse HEAD > config/gitcommit 
+cat config/gitcommit
